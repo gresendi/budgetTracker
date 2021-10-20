66eafa69881471b8d2339b6de9ba21071f551af9
Gilberto Resendiz <gresendi@uci.edu>
2021-10-19 22:25:34 -0700

    re-did server.js file

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const { join } = require('path')
+
 
 const PORT = 3000;
-
+const syncDB = require('./db')
 const app = express();
 
-app.use(logger("dev"));
 
-app.use(compression());
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-// routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+
+syncDB()
+  .then(() => app.listen(process.env.PORT || PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  }))
+  .catch(err => console.log(err))
